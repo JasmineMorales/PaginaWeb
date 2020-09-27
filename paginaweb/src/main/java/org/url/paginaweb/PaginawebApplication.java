2bdfe91b358defa1e6faa81c9d16c04b05b7435e
Marcos <sierramarcos146@gmail.com>
2020-09-27 13:56:52 -0600

    Agregando proyecto base

---
+++ paginaweb/src/main/java/org/url/paginaweb/PaginawebApplication.java
@@ -0,0 +1,13 @@
+package org.url.paginaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginawebApplication.class, args);
+	}
+
+}
