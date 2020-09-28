26a65308e19de1dd159bc9704fe95d5b01a31730
Marcos <sierramarcos146@gmail.com>
2020-09-28 17:13:32 -0600

    cambios

---
+++ paginaweb/src/main/java/org/url/paginaweb/configuraciones/SecurityConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.url.paginaweb.configuraciones;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author marcos
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("administrador")
+                .password("{noop}12345")
+                .roles("ADMIN")
+                .and()
+                .withUser("cliente")
+                .password("{noop}12345")
+                .roles("CLIENT")
+                .and()
+                .withUser("repartidor")
+                .password("{noop}12345")
+                .roles("REPART");
+    }
+}
