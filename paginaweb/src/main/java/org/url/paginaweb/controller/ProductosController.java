5fef8dc046bee2bb230cecde40d34d817c8e3367
Jasmine Morales <jazzimg123@gmail.com>
2020-10-08 19:39:06 -0600

    Diseño de vistas producto/controller
    Diseño de vistas de producto, lista de producto, comentarios, controlador

---
+++ paginaweb/src/main/java/org/url/paginaweb/controller/ProductosController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.url.paginaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+/**
+ *
+ * @author jasmine
+ */
+@Controller
+public class ProductosController {
+    
+    @GetMapping("/Productos/productos")
+    public String getProductPage(){
+        return("/Productos/producto");
+    }
+
+    
+            @GetMapping("/Productos/agregar_producto")
+    public String getCarritoPage(){
+        return("/Productos/agregar_producto");}
+
+        
+            @GetMapping("/Productos/producto")
+    public String getPagoCompraPage(){
+        return("/Productos/producto");
+    }
+}
