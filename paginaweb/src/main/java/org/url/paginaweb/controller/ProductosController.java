70bded6cde502fe295d275afedc24c7d2d98159b
Jasmine Morales <jazzimg123@gmail.com>
2020-10-27 17:24:06 -0600

    Js/Service/Controller

--- paginaweb/src/main/java/org/url/paginaweb/controller/ProductosController.java
+++ paginaweb/src/main/java/org/url/paginaweb/controller/ProductosController.java
@@ -5,10 +5,14 @@
  */
 package org.url.paginaweb.controller;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.url.paginaweb.modelo.Producto;
 import org.url.paginaweb.service.ProductService;
 /**
  *
@@ -22,23 +26,37 @@ public class ProductosController {
     private ProductService productService;
     
     @GetMapping("/Productos/productos")
-    public String getProductPage(){
+    public String getProductPage(Model model){
+        List productos = productService.getAllProducts();
+        model.addAttribute("productos", productos);
         return("/Productos/vistaProductos");
     }
-    
+           
         @GetMapping("Productos/modificar_producto")
     public String getEditProductPage(){
         return("/Productos/modificarProducto");
     }
     
-            @GetMapping("/Productos/agregar_producto")
+    @GetMapping("/Productos/agregar_producto")
     public String getAddProductPage(){
-        return("/Productos/agregarProducto");}
+        return("/Productos/agregarProducto");
+    }
+    
+           /* @PostMapping("/Productos/agregar_producto")
+    public String postAddProductPage(@ModelAttribute Producto producto, Model model){
+        model.addAttribute("producto", producto);
+        System.out.println(producto.getNombre());
+        System.out.println(producto.getPrecio());
+        productService.postProduct(producto);
+        model.addAttribute("productos", productService.getAllProducts().getResults());
+        return("/Productos/agregarProducto");}*/
 
         
             @GetMapping("/Productos/producto")
-    public String getProductListPage(){
-        //productService.getAllProducts();
+    public String getProductListPage(@RequestParam(name="variable1", required=true, defaultValue = "1") int id, Model model){
+        model.addAttribute("id", id);
+        Producto producto = productService.getProductID(id);
+        model.addAttribute("producto", producto);
         return("/Productos/producto");
     }
 }
--- paginaweb/src/main/java/org/url/paginaweb/service/ProductService.java
+++ paginaweb/src/main/java/org/url/paginaweb/service/ProductService.java
@@ -1,5 +1,6 @@
 package org.url.paginaweb.service;
 
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,19 +19,28 @@ public class ProductService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public Producto getProductID(Integer id) {
-        String url = "http://ec2-54-214-157-22.us-west-2.compute.amazonaws.com/api/v1.0/producto/" + id.toString() + "/";
+    public Producto getProductID(int id) {
+        String url = "http://ec2-54-214-157-22.us-west-2.compute.amazonaws.com/api/v1.0/producto/" + id + "/";
         Producto respuesta = restTemplate.getForObject(url, Producto.class);
-        return null;
+        return respuesta;
     }
 
-    public ArregloProducto getAllProducts() {
+    public List getAllProducts() {
         String url1 = "http://ec2-54-214-157-22.us-west-2.compute.amazonaws.com/api/v1.0/producto/";
         ArregloProducto response1 = restTemplate.getForObject(url1, ArregloProducto.class);
-        log.info(response1.toString());
-        if (response1.getCount() == 0) {
-            return null;
-        }
-        return response1;
+        List<Producto> respuesta = response1.getResults();
+        return respuesta;
+    }
+    
+    public void postProduct(Producto producto){
+        String url = "http://ec2-54-214-157-22.us-west-2.compute.amazonaws.com/api/v1.0/producto/";
+        Producto result = restTemplate.postForObject(url, producto, Producto.class);
+        System.out.println(result);
+    }
+    
+    public void putProduct(Producto producto){
+        String url = "http://ec2-54-214-157-22.us-west-2.compute.amazonaws.com/api/v1.0/producto/";
+        Producto result = restTemplate.postForObject(url, producto, Producto.class);
+        System.out.println(result);
     }
 }
